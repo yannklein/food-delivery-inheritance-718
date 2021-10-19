8d0e69e543beff1eecfd9e6ea1ca372902410c03
Douglas Berkley <douglasmberkley@gmail.com>
2021-10-19 18:23:05 +0900

    added inheritance

--- app.rb
+++ app.rb
@@ -8,9 +8,11 @@ csv_file = File.join(__dir__, 'data/meals.csv')
 meal_repository = MealRepository.new(csv_file)
 meals_controller = MealsController.new(meal_repository)
 
+p meal_repository.all.first.class.headers
+
 csv_file = File.join(__dir__, 'data/customers.csv')
 customer_repository = CustomerRepository.new(csv_file)
 customers_controller = CustomersController.new(customer_repository)
 
 router = Router.new(meals_controller, customers_controller)
-router.run
+# router.run
--- app/repositories/meal_repository.rb
+++ app/repositories/meal_repository.rb
@@ -1,43 +1,14 @@
 require 'csv'
 require_relative '../models/meal'
+require_relative 'base_repository'
 
-class MealRepository
-  def initialize(csv_file_path)
-    @csv_file_path = csv_file_path
-    @meals = [] # elements
-    @next_id = 1
-    load_csv if File.exist?(@csv_file_path)
-  end
-
-  def all
-    @meals
-  end
-
-  def create(meal)
-    meal.id = @next_id
-    @next_id += 1
-    @meals << meal
-    save_csv
-  end
+class MealRepository < BaseRepository
 
   private
 
-  def save_csv
-    CSV.open(@csv_file_path, 'wb') do |csv|
-      csv << ['id', 'name', 'price']
-      @meals.each do |meal|
-        csv << [meal.id, meal.name, meal.price]
-      end
-    end
-  end
-
-  def load_csv
-    csv_options = { headers: :first_row, header_converters: :symbol}
-    CSV.foreach(@csv_file_path, csv_options) do |attributes|
-      attributes[:id] = attributes[:id].to_i
-      attributes[:price] = attributes[:price].to_i
-      @meals << Meal.new(attributes)
-      @next_id += 1
-    end
+  def build_instance(attributes)
+    attributes[:id] = attributes[:id].to_i
+    attributes[:price] = attributes[:price].to_i
+    Meal.new(attributes)
   end
 end
